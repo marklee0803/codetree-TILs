38324332c263f7daff0884f0314a5e61c0a082bf
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-04-22 20:42:23 +0900

    Title: a 부터 b 까지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240422/a 부터 b 까지/a-to-b.py
+++ 240422/a 부터 b 까지/a-to-b.py
@@ -4,8 +4,12 @@ a = int(arr[0])
 b = int(arr[1])
 
 for i in range(a, b+1) :
+
+print(a,end=' ')
+
   if a % 2 == 1 :
      a *= 2 
   else : 
      a += 3
-  print(a,end=' ')
+  if a > b :
+     break
