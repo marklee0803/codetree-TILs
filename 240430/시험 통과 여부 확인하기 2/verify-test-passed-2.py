e85890987d34acdf3e6120af7ca6d5f72488a81f
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-04-30 11:27:32 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240430/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 240430/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -7,7 +7,7 @@ cnt = 0
 for i in range(학생수):
     arr = list(map(int, input().split()))   
     
-    if sum(arr)/학생수 >= 60 :
+    if sum(arr)/4 >= 60 :
          print('pass')
          cnt += 1
     else:
