aac6b1583eff3ce5db97cb3efe0d8bf2915bc48e
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-25 11:50:45 +0900

    Title: 입력받아 계산 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240625/입력받아 계산 2/input-calculate-2.py
+++ 240625/입력받아 계산 2/input-calculate-2.py
@@ -1,4 +1,15 @@
+c = input().split()
+a = int(c[0])
+b = int(c[1])
+print(a+b)
+
+
+
+
 c = int(input().split())
 a = c[0]
 b = c[1]
 print(a+b)
+
+
+#TypeError: int() argument must be a string, a bytes-like object or a number, not 'list'
