4a2707da1d1ab5abcdf1a801f17c67cb2464b24f
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-26 17:20:46 +0900

    Title: 특정 조건 세 정수 비교, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240626/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240626/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -3,9 +3,9 @@ a = int(temp[0])
 b = int(temp[1])
 c = int(temp[2])
 if a < b and a < c : 
-    print(1)
+    print(1,end=' ')
 else: 
-    print(0)
+    print(0, end=' ')
 
 if a == b and a ==c and b ==c :
     print(1)
