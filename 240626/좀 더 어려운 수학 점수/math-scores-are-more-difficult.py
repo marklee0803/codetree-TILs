c145dceb5414c158f729e14b6b3f2c6f27354c84
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-26 17:52:17 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240626/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,15 @@
+temp1 = input().split()
+temp2 = input().split()
+ae = int(temp1[0])
+am = int(temp[1])
+be = int(temp2[0])
+bm = int(temp2[1])
+
+if am  < bm : 
+    print('B')
+elif am > bm:
+    print('A')
+elif am == bm , ae > be :
+    print('A')
+else: 
+    print('B')
