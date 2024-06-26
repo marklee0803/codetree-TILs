efcc60fed32ecf9562ce228bf3c635ab48035db3
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-26 17:45:48 +0900

    Title: 장학금, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240626/장학금/scholarship.py
+++ 240626/장학금/scholarship.py
@@ -3,8 +3,8 @@ j = int(temp[0])
 g = int(temp[1])
 
 if j >= 90 and g >= 95 :
-    print('10만원')
+    print('100000')
 elif j >= 90 and g >= 90: 
-    print('5만원')
+    print('50000')
 else: 
     print(0)
