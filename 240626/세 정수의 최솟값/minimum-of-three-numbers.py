ace457080ab7a1d7a86c0cd82721ffe114c4f144
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-26 17:26:03 +0900

    Title: 세 정수의 최솟값, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240626/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -0,0 +1,11 @@
+temp = input().split()
+a = int(temp[0])
+b = int(temp[1])
+c = int(temp[2])
+
+if a <=b and a<= c :
+    print(a)
+elif b <=a and b<=c: 
+    print(b)
+elif c<=a and c<= b:
+    print(c)
