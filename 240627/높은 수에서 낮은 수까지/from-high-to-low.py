cdb8e0634ce3b7661a44708754d4db51d6eab235
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-27 10:43:41 +0900

    Title: 높은 수에서 낮은 수까지, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240627/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,9 @@
+temp = input().split()
+a = int(temp[0])
+b = int(temp[1])
+if a>= b:
+    for i in range(a, b-1, -1):
+        print(i,end=' ')
+else:
+    for i in range(b, a-1, 1):
+        print(i, end=' ')
