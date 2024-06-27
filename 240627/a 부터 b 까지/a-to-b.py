bce84c09ab56eea4fcbdedc870a2da8468521045
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-27 11:18:09 +0900

    Title: a 부터 b 까지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240627/a 부터 b 까지/a-to-b.py
+++ 240627/a 부터 b 까지/a-to-b.py
@@ -2,9 +2,8 @@ temp = input().split()
 a = int(temp[0])
 b = int(temp[1])
 while a <= b:
+    print(a)    
     if a % 2 == 1:
         a *= 2
-    print(a)
     else: 
-        a+=3 
-    print(a)
+        a +=
