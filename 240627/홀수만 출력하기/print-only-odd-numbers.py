35d01b29ef6b5ba604f5587743466e051b16f5dd
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-27 11:35:46 +0900

    Title: 홀수만 출력하기, Time: 154ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240627/홀수만 출력하기/print-only-odd-numbers.py
+++ 240627/홀수만 출력하기/print-only-odd-numbers.py
@@ -3,6 +3,5 @@ n = int(input())
 for i in range(n):
     a = int(input())
     
-for i in range(1,n+1):
-    if i % 2 == 1 or i % 3 == 0 :
-        print(i)
+    if a % 2 == 1 or a % 3 == 0 :
+        print(a)
