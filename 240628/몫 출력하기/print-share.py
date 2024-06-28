f0a7fe7f96d92fe055fe0cade1b547d0b6b9a85f
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 19:30:36 +0900

    Title: 몫 출력하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240628/몫 출력하기/print-share.py
+++ 240628/몫 출력하기/print-share.py
@@ -1,7 +1,11 @@
-for i in range(3):
+k = 0
+while True:
     n = int(input())
     if n % 2 == 1:
         continue
     else:
         m = n // 2
         print(m)
+        k += 1
+        if k == 3:
+            break
