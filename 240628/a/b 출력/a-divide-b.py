870c739a083fecf06c0a3834e1c74ab6ab030017
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 20:31:44 +0900

    Title: a/b 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240628/a/b 출력/a-divide-b.py
+++ 240628/a/b 출력/a-divide-b.py
@@ -3,8 +3,9 @@ a = int(변수[0])
 b = int(변수[1])
 
 
-몫 = a // b 
+print(f'{a//b}.",end='')
+a %= b 
 for i in range(20):
-   p = 10 * a % b 
-   p *= 10 
-   print(p)
+   a *= 10
+   print(a//b, end='')
+   a % = b
