f725da11dcdf614b3d23325d94a0bae7c67258a9
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 22:02:21 +0900

    Title: ab 사이에 없는 c, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240628/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240628/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -2,12 +2,12 @@ temp = input().split()
 a = int(temp[0])
 b = int(temp[1])
 c = int(temp[2])
-c = True
+w = True
 for i in range(a, b+1):
     if i % c == 0:
         c = False
     
-if c == True:
+if w == True:
     print('YES')
 else:
     print('NO')
