87bb4b53dedbea685d17e50e66f5557f68c7326c
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 18:01:25 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240628/369 게임/369-game.py
+++ 240628/369 게임/369-game.py
@@ -1,6 +1,2 @@
-n = int(input())
-for i in range(1, n+1):
-    if i % 3 == 0 or '3' or '6' or '9' in str(n):
-        print(0, end=' ')
-    else: 
-        print(i, end=' ')
+a =('3' or '6' or '9' )in str(n)
+print(a)
