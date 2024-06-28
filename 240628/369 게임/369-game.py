4005270789f6e01d55bbdbee297af404786d9821
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 18:02:01 +0900

    Title: 369 게임, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240628/369 게임/369-game.py
+++ 240628/369 게임/369-game.py
@@ -1,2 +1,6 @@
-a =('3' or '6' or '9' )in str(n)
-print(a)
+n = int(input())
+for i in range(1, n+1):
+    if i % 3 == 0 or ('3' or '6' or '9') in str(i):
+        print(0, end=' ')
+    else: 
+        print(i, end=' ')
