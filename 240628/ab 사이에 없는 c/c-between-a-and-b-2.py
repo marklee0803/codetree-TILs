19a59539d21adb02c7d5340ed8f3794e11035887
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 22:04:41 +0900

    Title: ab 사이에 없는 c, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240628/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240628/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -3,9 +3,10 @@ a = int(temp[0])
 b = int(temp[1])
 c = int(temp[2])
 w = True
+
 for i in range(a, b+1):
     if i % c == 0:
-        c = False
+        w = False
     
 if w == True:
     print('YES')
