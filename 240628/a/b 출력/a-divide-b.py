6319d6671503049c64f4f08d737d2ddbc6a64208
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 20:13:34 +0900

    Title: a/b 출력, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240628/a/b 출력/a-divide-b.py
+++ 240628/a/b 출력/a-divide-b.py
@@ -3,8 +3,9 @@ a = int(변수[0])
 b = int(변수[1])
 
 
-
+몫 = a % b 
 for i in range(20):
-   k = (a // b) * 10 % b 
-   k = 10 * k - b * k
+   k = (a // b) * 10 // b 
+   p = (a // b) * 10 % b
+   k = p
    print(k)
