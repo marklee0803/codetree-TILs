08d5f7ea99da451795ace869728a4a4001d560b6
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-06-28 10:21:58 +0900

    Title: 숫자 사이의 배수, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240628/숫자 사이의 배수/multiplication-between-numbers.py
+++ 240628/숫자 사이의 배수/multiplication-between-numbers.py
@@ -2,7 +2,9 @@ temp = input().split()
 a = int(temp[0])
 b = int(temp[1])
 sum = 0
+cnt = 0
 for i in range(a, b+1):
     if i % 5 == 5 or i % 7 == 0:
         sum += i
-print(sum)
+        cnt += 1
+print(sum, sum/cnt)
