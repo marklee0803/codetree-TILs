7a7f0877770a3dea9fa461e4393624c221cef1c9
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-01 19:04:55 +0900

    Title: 별 그리기, Time: 151ms, Memory: 27MB, Status: Passed - CodeTree

--- 240701/별 그리기/star-drawing.py
+++ 240701/별 그리기/star-drawing.py
@@ -10,6 +10,6 @@ for i in range(n):
 for i in range(n-1):
     for j in range(i+1):
         print(' ',end='')
-    for j in range(-2*i+n):
+    for j in range(-2*i+2*n-3):
         print('*',end='')
     print()
