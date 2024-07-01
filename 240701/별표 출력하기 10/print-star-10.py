713150fe8a71423f4e1be88a05b7778e339cfd55
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-01 20:54:57 +0900

    Title: 별표 출력하기 10, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240701/별표 출력하기 10/print-star-10.py
+++ 240701/별표 출력하기 10/print-star-10.py
@@ -2,10 +2,10 @@ n = int(input())
 
 for i in range(2*n):
     if i % 2 == 0:
-        for j in range((i/2)+1):
+        for j in range((i//2)+1):
             print('*',end=' ')
         print()
     else:
-        for j in range(n-(i-1)/2):
+        for j in range(n-(i-1)//2):
             print('*',end=' ')
         print()
