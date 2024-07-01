175d9212e7d7a4205de2d24d210a65d7ca25d8c9
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-01 20:49:20 +0900

    Title: 별표 출력하기 10, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240701/별표 출력하기 10/print-star-10.py
+++ 240701/별표 출력하기 10/print-star-10.py
@@ -1,10 +1,11 @@
 n = int(input())
-for i in range(n):
+
+for i in range(2*n):
     if i % 2 == 0:
-        for j in range(i+1):
+        for j in range(i/2+1):
             print('*',end=' ')
         print()
     else:
-        for j in range(n-i):
+        for j in range(n-(i-1)/2):
             print('*',end=' ')
         print()
