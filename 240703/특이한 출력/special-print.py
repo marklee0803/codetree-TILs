92505e6ea9bf642cb22cc08b6f0d6ef31aa6cec6
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-03 23:04:27 +0900

    Title: 특이한 출력, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240703/특이한 출력/special-print.py
+++ 240703/특이한 출력/special-print.py
@@ -4,4 +4,4 @@ for i in range(1, n+1):
         if (i+j) % 4 == 0:
             print(f'({i}, {j})',end='\n')
         else:
-            print(f'({i},{j})',end=' ')
+            print(f'({i}, {j})',end=' ')
