40713691a7054737593082c09f96df632ca1b70e
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-03 23:08:10 +0900

    Title: 19단 출력, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240703/19단 출력/nineteen-times-table.py
+++ 240703/19단 출력/nineteen-times-table.py
@@ -1,6 +1,6 @@
 for i in range(1, 20):
     for j in range(1, 20):
-        if j % 2 == 1:
+        if j % 2 == 1 and j != 19 :
             print(f'{i} * {j} = {i*j}',end=' ')
             print('/',end=' ')
         else:
