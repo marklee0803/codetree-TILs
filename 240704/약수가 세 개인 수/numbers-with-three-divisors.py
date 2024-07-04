6f091d31fdcb5d7ff9f4973bef46f0b35e089524
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-04 15:30:01 +0900

    Title: 약수가 세 개인 수, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240704/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 240704/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -1,9 +1,10 @@
 temp = input().split()
 start, end = int(temp[0]), int(temp[1])
-cnt = 0
 cnt2 = 0
 for i in range(start,end+1):
+    cnt = 0
     for j in range(1, i+1):
+        
         if i % j == 0:
             cnt += 1
 
