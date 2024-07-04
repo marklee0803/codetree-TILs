09fe9457174af13cc8bf4051cb6726b7bc0340b8
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-04 15:52:04 +0900

    Title: 짝수의 합 n번 구하기, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240704/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
+++ 240704/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
@@ -3,7 +3,7 @@ for i in range(n):
     temp = input().split()
     a , b = int(temp[0]),int(temp[1])
     sum = 0
-    for j in range(a, b+1, 2):
+    for j in range(a, b+1):
         if j % 2 == 0:
             sum += j
            
