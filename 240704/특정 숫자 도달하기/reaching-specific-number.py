36cf0af89de289b3b56fcdc827295174dfc28a72
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-04 18:32:09 +0900

    Title: 특정 숫자 도달하기, Time: 151ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/특정 숫자 도달하기/reaching-specific-number.py
+++ 240704/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,9 +1,11 @@
 arr = list(map(int, input().split()))
 sum = 0 
 cnt = 0
-for i in range(len(arr)) :
-    sum += arr[i] 
+new_list = []
+for i in arr:
+    if i <= 250:
+        new_list.append(i) 
+for i in new_list:
+    sum += i
     cnt += 1
-    if arr[i] >= 250:
-        arr.pop(i)
 print(f'{sum} {sum/cnt:.1f}')
