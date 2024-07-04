982a792a4a7ca12b6d8653ddfe9b6c2044eb8879
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-04 15:15:58 +0900

    Title: 완전수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/완전수/perfect-number.py
@@ -0,0 +1,11 @@
+temp = input().split()
+start , end= int(temp[0]), int(temp[1])
+cnt = 0
+cnt2 = 0
+for i in range(start, end+1):
+    for j in range(1, i):
+        if i % j == 0:
+            cnt += j
+    if cnt == i :
+        cnt2 += 1
+print(cnt2)
