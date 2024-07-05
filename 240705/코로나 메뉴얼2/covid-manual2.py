6ac81894e1b78cee7ba75d58f82e41daf9e8b61b
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-05 20:52:48 +0900

    Title: 코로나 메뉴얼2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240705/코로나 메뉴얼2/covid-manual2.py
+++ 240705/코로나 메뉴얼2/covid-manual2.py
@@ -3,7 +3,27 @@ arr2 = input().split()
 arr3 = input().split()
 cnt_arr = [0, 0 , 0, 0]
 for i in range(1,4):
-    for elem in arr+i:
+    for elem in arr1:
+        if elem[0] ==  'Y' and int(elem[1]) >= 37:
+            cnt_arr[0] += 1
+        
+        elif elem[0]== 'N' and int(elem[1]) >= 37:
+            cnt_arr[1] += 1
+        elif elem[0] == 'Y' :
+            cnt_arr[2] += 1
+        else:
+            cnt_arr[3] += 1
+    for elem in arr2:
+        if elem[0] ==  'Y' and int(elem[1]) >= 37:
+            cnt_arr[0] += 1
+        
+        elif elem[0]== 'N' and int(elem[1]) >= 37:
+            cnt_arr[1] += 1
+        elif elem[0] == 'Y' :
+            cnt_arr[2] += 1
+        else:
+            cnt_arr[3] += 1
+    for elem in arr3:
         if elem[0] ==  'Y' and int(elem[1]) >= 37:
             cnt_arr[0] += 1
         
