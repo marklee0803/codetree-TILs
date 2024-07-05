bde12dde9f596891589fe9fe726805c925c017ba
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-05 17:02:39 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240705/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,9 @@
+n = int(input())
+cnt = 0
+for i in range(n):
+    arr = list(map(int , input().split()))
+    if sum(arr) / 4 >= 60:
+        print('pass')
+        cnt += 1
+    else:
+        print('fail')
