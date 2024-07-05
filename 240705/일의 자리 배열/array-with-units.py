0e4b8b013e4b347cdc9ee349a071cdad8b58fa2b
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-05 17:51:03 +0900

    Title: 일의 자리 배열, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240705/일의 자리 배열/array-with-units.py
+++ 240705/일의 자리 배열/array-with-units.py
@@ -3,9 +3,5 @@ for i in range(3, 11):
     arr.append(arr[-1]+arr[-2])
 
 for elem in arr:
-    if elem < 10 :
-        print(elem,end=' ')
-    elif elem < 100:
-        print(elem-10,end=' ')
-    elif elem < 1000:
-        print(elem-100,end=' ')
+    
+        print(elem//10,end=' ')
