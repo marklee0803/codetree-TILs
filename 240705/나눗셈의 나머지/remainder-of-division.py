53af829c2c38002a07afddd6a468b25d4b96e289
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-05 21:20:54 +0900

    Title: 나눗셈의 나머지, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240705/나눗셈의 나머지/remainder-of-division.py
+++ 240705/나눗셈의 나머지/remainder-of-division.py
@@ -2,16 +2,17 @@ temp = input().split()
 a = int(temp[0])
 b = int(temp[1])
 
-cnt_arr = [0]*(b)
+cnt_arr = [0]*(b-1)
 
 while a >1 :
     t = a // b
     k = a % b
     a = t 
-    for i in range(1, b):
+    for i in range(0, b-1):
         if k == i:
             cnt_arr[i] += 1 
 sum = 0
+print(cnt_arr)
 for i in cnt_arr:
     sum += i**2
 print(sum)
