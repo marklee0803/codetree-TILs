407de7eb5125fc2287228f813120d27f559dbe40
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-07 18:19:09 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240707/배열 놀이/play-with-array.py
@@ -0,0 +1,14 @@
+temp = input().split()
+n, p = int(temp[0]), int(temp[1])
+arr = list(map(int, input().split()))
+temp2 = input().split()
+a = int(temp2[1])
+temp3 = input().split()
+b = int(temp3[1])
+temp4 = input().split()
+s ,e = int(temp[1]) , int(temp[2])
+
+print(arr[a-1]))
+print(index(b)+1)
+for elem in arr[s:e+1]:
+    print(elem, end=' ')
