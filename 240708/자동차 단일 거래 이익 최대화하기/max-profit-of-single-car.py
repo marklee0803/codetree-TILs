004abfbf87cfc2653c63905b52b8ccdd699109a4
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 18:52:25 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240708/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+new_arr = []
+for i in range(n):
+    a = arr[i+1] - arr[i]
+    new_arr.append(a)
+
+max_arr = new_arr[0]
+for elem in new_arr:
+    if max_arr < elem:
+        max_arr = elem
+print(max_arr)
