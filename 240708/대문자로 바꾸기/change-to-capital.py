463832ef83a15558962bd4e3c369fa0f79e61975
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 20:39:15 +0900

    Title: 대문자로 바꾸기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240708/대문자로 바꾸기/change-to-capital.py
+++ 240708/대문자로 바꾸기/change-to-capital.py
@@ -1,8 +1,8 @@
 arr = [input().split() for _ in range(5)]
-cnt = 65
+
 for i in range(5):
     for j in range(3):
-        arr[i][j] = chr(cnt)
+        arr[i][j] = chr(ord(arr[i][j]))
         print(arr[i][j],end=' ')
         cnt += 1
     print()
