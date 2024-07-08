c25d776cb1ee25ba2e62f88a7924b8f236f444cb
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 21:03:21 +0900

    Title: 배열의 평균, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240708/배열의 평균/ave-of-array.py
@@ -0,0 +1,18 @@
+arr = [list(map(int,input().split())) for i in range(4)]
+sum1 = 0
+sum2 = 0 
+sum3 = 0
+
+for i in range(4):
+    print(f'{sum(arr[i])/2:.1f}')
+sum = 0
+for i in range(2):
+    for j in range(4):
+        sum += arr[j][i]
+        print(f'{sum(arr[j][i])}/4')
+    print(f'{sum/4:.1f}')
+
+sum2 = 0
+for i in range(4):
+    sum2 += arr[i]
+print(f'{sum2/8:.1f})
