3976cf461518b4554a16a8b72f604f601b579524
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 17:31:12 +0900

    Title: n개의 숫자 중 최대 2개, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240708/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+max_arr = arr[0]
+for elem in arr:
+    if elem > max_arr:
+        max_arr = elem
+
+arr.pop(max_arr)
+
+second_max_arr = arr[0]
+for elem in arr:
+    if elem > second_max_arr:
+        second_max_arr = elem
+
+print(max_arr, second_max_arr)
