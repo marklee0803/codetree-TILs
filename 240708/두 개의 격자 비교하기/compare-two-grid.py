7920724cfe88082617db13f89800dc8803ef34e3
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 22:17:57 +0900

    Title: 두 개의 격자 비교하기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/두 개의 격자 비교하기/compare-two-grid.py
+++ 240708/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,7 +1,7 @@
 temp = input().split()
 n , m = int(temp[0]), int(temp[1])
 arr1 = [list(map(int, input().split())) for _ in range(n)]
-input()
+
 arr2 = [list(map(int, input().split())) for _ in range(n)]
 arr = [[0 for _ in range(m)] for _ in range(n)]
 
