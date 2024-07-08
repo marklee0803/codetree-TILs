9527e4582a57f200d6ac4c486f3bff500d5da1b6
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 21:57:05 +0900

    Title: 숫자 직사각형, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240708/숫자 직사각형/rectangle-of-numbers.py
+++ 240708/숫자 직사각형/rectangle-of-numbers.py
@@ -1,11 +1,11 @@
 temp = input().split()
 n , m = int(temp[0]) , int(temp[1])
 
-arr = [[0 for i in range(5)] for i in range(3)]
+arr = [[0 for i in range(m)] for i in range(n)]
 
 num = 1 
-for i in range(3):
-    for j in range(5):
+for i in range(n):
+    for j in range(m):
         arr[i][j] = num
         num += 1
 
