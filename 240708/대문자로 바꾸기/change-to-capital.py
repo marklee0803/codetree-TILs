2d95559bd1126d3985b413dd1d2554abc8f13e14
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 20:41:35 +0900

    Title: 대문자로 바꾸기, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/대문자로 바꾸기/change-to-capital.py
+++ 240708/대문자로 바꾸기/change-to-capital.py
@@ -2,7 +2,7 @@ arr = [input().split() for _ in range(5)]
 
 for i in range(5):
     for j in range(3):
-        arr[i][j] = chr(ord(arr[i][j]))
+        arr[i][j] = chr(ord(arr[i][j])+ ord('A') - ord('a'))
         print(arr[i][j],end=' ')
         
     print()
