e6aabd9deaa0ab1ccbd4039aff99d172f41e4623
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 21:56:29 +0900

    Title: 숫자 직사각형, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240708/숫자 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,15 @@
+temp = input().split()
+n , m = int(temp[0]) , int(temp[1])
+
+arr = [[0 for i in range(5)] for i in range(3)]
+
+num = 1 
+for i in range(3):
+    for j in range(5):
+        arr[i][j] = num
+        num += 1
+
+for row in arr:
+    for elem in row:
+        print(elem, end=' ')
+    print()
