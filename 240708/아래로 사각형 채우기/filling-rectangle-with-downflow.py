09fbf76514f4a8490c1453649d6cb480388dd39f
marklee0803 <165676483+marklee0803@users.noreply.github.com>
2024-07-08 22:24:27 +0900

    Title: 아래로 사각형 채우기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/아래로 사각형 채우기/filling-rectangle-with-downflow.py
+++ 240708/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -1,10 +1,11 @@
 n = int(input())
 arr = [[0 for i in range(n)]for i in range(n)]
 
-num = 0
+
 for i in range(n):
+    num = 0
     for j in range(n):
-        arr[i][j] = i + num 
+        arr[i][j] = i+1 + num 
         num += 3 
 
 for row in arr:
